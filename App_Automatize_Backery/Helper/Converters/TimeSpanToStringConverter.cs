7df409364a60165adea6cdd68226ab3521981d25
Аверкин Сергей1 <serezhenkaaverkin@mail.ru>
2025-04-06 03:16:22 +0300

    Редактирование конвертера для перевода TS в DT, настройка отображения времени в окнах создания.

--- App_Automatize_Backery/Helper/Converters/TimeSpanToStringConverter.cs
+++ App_Automatize_Backery/Helper/Converters/TimeSpanToStringConverter.cs
@@ -4,30 +4,29 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace App_Automatize_Backery.Helper.Converters
 {
-    internal class TimeSpanToDateTimeConverter : IValueConverter
+    public class TimeSpanToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is TimeSpan timeSpan)
             {
-                // Преобразуем TimeSpan в DateTime и форматируем для 24-часового отображения
-                DateTime time = DateTime.Today.Add(timeSpan);
-                return time.ToString("HH:mm", culture); // 24-часовой формат
+                return timeSpan.ToString(@"hh\:mm");
             }
-            return null;
+            return string.Empty;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is DateTime dateTime)
+            if (TimeSpan.TryParseExact(value as string, @"hh\:mm", CultureInfo.InvariantCulture, out var timeSpan))
             {
-                return dateTime.TimeOfDay; // Преобразуем DateTime обратно в TimeSpan
+                return timeSpan;
             }
-            return null;
+            return DependencyProperty.UnsetValue;
         }
     }
 }
